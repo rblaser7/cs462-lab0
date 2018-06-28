bf20d5683fefde0f41504b90d77460249b99b613
Ryan Blaser <ryan.blaser@familysearch.org>
2018-06-28 12:25:14 -0600

    Update userData.sh

--- userData.sh
+++ userData.sh
@@ -9,4 +9,4 @@ chown -R ec2-user:apache /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
-mv index.html /var/www/index.html
+mv index.html /var/www/html/index.html
